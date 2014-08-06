205413bc25bd8da8b7a4a991afc9eed342827736
ZIM555 <zim555@mail.ru>
2014-08-06 11:04:47 +0300

    Init from 7272 device

---
+++ cm.mk
@@ -0,0 +1,19 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := GT-S7270
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/logan/device_logan.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := logan
+PRODUCT_NAME := cm_logan
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-S7270
+
