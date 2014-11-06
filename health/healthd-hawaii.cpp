6b3488279f19190a8d322d40247e3d87ddcbac12
ZIM555 <zim555@mail.ru>
2014-11-06 19:57:51 +0200

    Fix ofline battary

--- health/healthd-hawaii.cpp
+++ health/healthd-hawaii.cpp
@@ -24,7 +24,7 @@ healthd_board_init(struct healthd_config *config)
     config->batteryStatusPath      = "/sys/class/power_supply/battery/status";
     config->batteryHealthPath      = "/sys/class/power_supply/battery/health";
     //config->batteryPresentPath     = "/sys/class/power_supply/battery/present";
-    config->batteryCapacityPath    = "/sys/class/power_supply/battery/capacity";
+    //config->batteryCapacityPath    = "/sys/class/power_supply/battery/capacity";
     config->batteryVoltagePath     = "/sys/class/power_supply/battery/voltage_now";
     config->batteryTemperaturePath = "/sys/class/power_supply/battery/temp";
     config->batteryTechnologyPath  = "/sys/class/power_supply/battery/technology";
